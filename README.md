007a6a7616fe93b2f3a1fce777405598ec40b29b
BonusDucksMkII <indifferential.equations@gmail.com>
2024-02-11 00:42:15 -0700

    Commit with finished program

---
+++ README.md
@@ -0,0 +1,28 @@
+# Overview
+
+Hello. This is a test program where I tried to learn more about Rust as a language, as well as better programming practices.
+
+This program leverages Rust's standard file I/O functions to open a file and get bytes out of it.
+
+As mentioned above, I wrote this to learn more about Rust as a language, which in turn has taught me more about good programming practices.
+
+Here's a short video detailed the program as well as what I learned:
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I used the Rust std library entirely on this project.
+
+I used VS Studio and one of the many Rust extentions.
+
+# Useful Websites
+
+- [TutorialsPoint](https://www.tutorialspoint.com/rust/)
+- [Rust Official Docs](https://doc.rust-lang.org/)
+
+# Future Work
+
+- Add ability to write to file
+- Write over statements found in file with regexp
+- Add some better error handling
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::env;
+use std::fs;
+
+/*
+TODO:
+    Find magic numbers in files
+ */
+
+/*
+NOTES
+Declare variable types with ':', like ':f32' or ':i8' or ':char'
+Macros are referenced with '!', like 'println!'
+Variables are immutable, apply 'mut' to change em
+*/
+
 fn main() {
-    println!("Hello, world!");
+    // collects arguments given into a vector (array)
+    let args: Vec<String> = env::args().collect();
+
+    // String is dynamically allocated; str is fixed. '&' is the reference operator (references value, not object itself)
+    let cmd_args = parse_args(&args);
+
+    // Rust expects Result enum to have error handling; make sure it does.
+    let contents = fs::read(&cmd_args.file_path)
+        .expect("Couldn't find file");
+    // No real error handling right now
+
+    println!("{:?}", contents);
+
+    // Get first four bytes of file
+    let first_bytes: &[u8] = &contents[0..4];
+
+    // iter: for each slice of first_bytes do what's inside brackets
+    for byte in first_bytes.iter(){
+        println!("0x{:x}", byte);
+    }
+    print_file_info(&cmd_args.file_path);
+}
+
+// All of this is from following the guide on modularity from the rust docs
+struct CmdArguments {
+    file_path: String
+}
+
+// -> operator is for return types (a bit confusing)
+fn parse_args(args: &[String]) -> CmdArguments{
+    let file_path;
+    // clone method is similar to '&', but just clones the value of an object
+    file_path = args[1].clone();
+    CmdArguments{ file_path }
+}
+
+fn print_file_info(file_path: &String) {
+    let metadata = fs::metadata(file_path)
+        .expect("Couldn't find file");
+    println!("{:?}", metadata.file_type());
+    println!("File is: {} bytes long", metadata.len());
 }
